0bdaf5d6682607f2bec456ebcc27415c1ef3a0b4
yoshikawa(Taiki Yoshikawa) <yoshikawataiki@gmail.com>
2022-05-11 01:43:20 +0900

    update siteMetadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -20,8 +20,8 @@ const siteMetadata = {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
     // supports plausible, simpleAnalytics, umami or googleAnalytics
-    plausibleDataDomain: 'yoshikawa.dev', // e.g. tailwind-nextjs-starter-blog.vercel.app
-    simpleAnalytics: true, // true or false
+    plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
+    simpleAnalytics: false, // true or false
     umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
     googleAnalyticsId: 'G-868L206KE4', // e.g. UA-000000-2 or G-XXXXXXX
   },
